fb6d6e6d6268f2631e07e909e095d0adb7b266f6
Admin <reteamboss@gmail.com>
2024-08-26 17:44:38 +0300

    Задание 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+
+    val array = arrayOf(
+        arrayOf(1,2,3,4,5),
+        arrayOf(6,7,8,9,90),
+        arrayOf(23,34,45,56,76),
+        arrayOf(34,54,23,34,45)
+    )
+
+    val k = 3
+
+    for (num in array[k-1]){
+        println(num)
+    }
 }
